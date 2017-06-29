3162b4c5bd93a74198ee6021bd5c971e2522825c
Srija <srija326@gmail.com>
2017-06-29 23:16:32 +0530

    Some basic structure and file reading.

--- src/main/java/com/mikosama/app/App.java
+++ src/main/java/com/mikosama/app/App.java
@@ -1,13 +1,68 @@
 package com.mikosama.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+class Slot
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+}
+
+class ParkingLot
+{
+	int nextParkingLocation = 0;
+	Car[] slots; //are we really using an array for this?
+
+	ParkingLot(int size)
+	{
+		slots = new Car[size];
+	}
+
+}
+
+class Car
+{
+
+}
+
+class Shell
+{
+	BufferedReader br = null;
+
+	Shell(String filename)
+	{
+		try
+		{
+			br = new BufferedReader(new FileReader(filename);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	Shell()
+	{
+		try
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void run()
+	{
+
+	}
+
+}
+
+public class App
+{
+public static void main( String[] args )
+{
+System.out.println( "Hello World!" );
+}
 }
